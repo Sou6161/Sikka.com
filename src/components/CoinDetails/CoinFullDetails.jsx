51b81b58568c4d31e7bb55ebff85b280feef4207
Sourabh Saini <sourabh61saini@gmail.com>
2024-09-12 19:16:41 +0530

    Changes Done

--- src/components/CoinDetails/CoinFullDetails.jsx
+++ src/components/CoinDetails/CoinFullDetails.jsx
@@ -25,22 +25,6 @@ const formatXAxis = (unixTimestamp) => {
     .padStart(2, "0")}`;
 };
 
-const CustomTooltip = ({ active, payload, label }) => {
-  if (active && payload && payload.length) {
-    const date = new Date(label);
-    const formattedPrice = payload[0].value / 1000;
-    return (
-      <div className="border border-gray-200 p-2 rounded shadow">
-        <p className="text-sm">{`${date.toLocaleString()}`}</p>
-        <p className="text-sm font-bold">{`Price: ${formattedPrice.toFixed(
-          1
-        )}K`}</p>
-      </div>
-    );
-  }
-  return null;
-};
-
 const formatPrice = (price) => {
   if (price >= 100) {
     return price.toFixed(2);
@@ -56,13 +40,53 @@ const CoinFullDetails = () => {
   const [showDropdown, setShowDropdown] = useState(false);
   const [CoinDetails, setCoinDetails] = useState(null);
   const [CoinChartDetails, setCoinChartDetails] = useState(null);
+  const [CoinMcapDetails, setCoinMcapDetails] = useState(null);
   const [timeFrame, setTimeFrame] = useState("24h");
+  const [chartType, setChartType] = useState("price");
+  const [lowestMarketCap, setLowestMarketCap] = useState(0);
+  const [highestMarketCap, setHighestMarketCap] = useState(0);
+  const [xAxisDomain, setXAxisDomain] = useState([0, 0]);
+
   const priceUSD = CoinDetails?.market_data?.current_price.usd;
   const priceBTC = CoinDetails?.market_data?.current_price.btc;
   const priceChange24h = CoinDetails?.market_data?.price_change_percentage_24h;
 
   console.log(id, "iouhouhgghuoggoo");
 
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      const date = new Date(label);
+      const formattedDate =
+        timeFrame === "7d"
+          ? `${date.toLocaleDateString("en-US", {
+              month: "short",
+              day: "2-digit",
+            })} ${date.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}`
+          : timeFrame === "1y"
+          ? `${date.toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            })} ${date.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}`
+          : date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+      const formattedPrice = payload[0].value / 1000;
+      return (
+        <div className="border border-gray-200 p-2 rounded shadow">
+          <p className="text-sm">{formattedDate}</p>
+          <p className="text-sm font-bold">{`Price: ${formattedPrice.toFixed(
+            1
+          )}K`}</p>
+        </div>
+      );
+    }
+    return null;
+  };
   const Dashboard = ({ CoinChartDetails, lowPrice, highPrice, tickValues }) => {
     // Check if CoinChartDetails is null or empty
     if (!CoinChartDetails || CoinChartDetails.length === 0) {
@@ -120,6 +144,7 @@ const CoinFullDetails = () => {
             return Math.round((lowestPrice + step * i) / 1000) * 1000;
           })
       : [];
+
   useEffect(() => {
     const FetchCoinDetails = async () => {
       const response = await fetch(
@@ -139,8 +164,27 @@ const CoinFullDetails = () => {
 
   useEffect(() => {
     const FetchCoinChart = async () => {
-      const days = timeFrame === "24h" ? 1 : 7;
-      let precision; // Define precision here
+      let days;
+      switch (timeFrame) {
+        case "24h":
+          days = 1;
+          break;
+        case "7d":
+          days = 7;
+          break;
+        case "1m":
+          days = 30;
+          break;
+        case "3m":
+          days = 90;
+          break;
+        case "1y":
+          days = 365;
+          break;
+        default:
+          days = 7;
+      }
+      let precision;
       const price = CoinDetails?.market_data?.current_price?.usd;
       if (price < 0.0001) {
         precision = 10;
@@ -151,7 +195,6 @@ const CoinFullDetails = () => {
       } else {
         precision = 2;
       }
-      // Add a default value for precision
       precision = precision || 2;
       const chartResponse = await fetch(
         `https://api.coingecko.com/api/v3/coins/${id}/ohlc?vs_currency=usd&days=${days}&precision=${precision}`,
@@ -166,6 +209,85 @@ const CoinFullDetails = () => {
   useEffect(() => {
     CoinChartDetails && console.log(CoinChartDetails);
   }, [CoinChartDetails]);
+
+  useEffect(() => {
+    const FetchCoinMCap = async () => {
+      let days;
+      switch (timeFrame) {
+        case "24h":
+          days = 1;
+          break;
+        case "7d":
+          days = 7;
+          break;
+        case "1m":
+          days = 30;
+          break;
+        case "3m":
+          days = 90;
+          break;
+        case "1y":
+          days = 365;
+          break;
+        default:
+          days = 7;
+      }
+      const response = await fetch(
+        `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${days}&interval=${
+          days === 1 ? "" : "daily"
+        }&precision=${getPrecision(lowestMarketCap)}`,
+        CoinGeckoYogeshApi
+      );
+      const CoinMCapData = await response.json();
+      if (CoinMCapData && CoinMCapData.market_caps) {
+        setCoinMcapDetails(CoinMCapData.market_caps);
+        const lowest = Math.min(
+          ...CoinMCapData.market_caps.map((item) => item[1])
+        );
+        setLowestMarketCap(lowest);
+        const highest = Math.max(
+          ...CoinMCapData.market_caps.map((item) => item[1])
+        );
+        setHighestMarketCap(highest);
+        const minTime = Math.min(
+          ...CoinMCapData.market_caps.map((item) => item[0])
+        );
+        const maxTime = Math.max(
+          ...CoinMCapData.market_caps.map((item) => item[0])
+        );
+        setXAxisDomain([minTime, maxTime]);
+      }
+    };
+    FetchCoinMCap();
+  }, [timeFrame, id, CoinGeckoYogeshApi]);
+
+  const getPrecision = (value) => {
+    if (value < 0.0000000001) {
+      return "10";
+    } else if (value < 0.00000001) {
+      return "9";
+    } else if (value < 0.0000001) {
+      return "8";
+    } else if (value < 0.000001) {
+      return "7";
+    } else if (value < 0.00001) {
+      return "6";
+    } else if (value < 0.0001) {
+      return "5";
+    } else if (value < 0.001) {
+      return "4";
+    } else if (value < 0.01) {
+      return "3";
+    } else if (value < 0.1) {
+      return "2";
+    } else {
+      return "0";
+    }
+  };
+  useEffect(() => {
+    CoinMcapDetails && console.log(CoinMcapDetails);
+  }, [CoinMcapDetails]);
+
   return (
     <>
       <div>
@@ -379,60 +501,200 @@ const CoinFullDetails = () => {
             {CoinDetails?.name} Price Chart{" "}
             {`(${CoinDetails?.symbol?.toUpperCase()})`}
           </h1>
-
-          <button
-            class="button TimeGraph mt-5"
-            onClick={() => setTimeFrame("24h")}
-          >
-            24h
-          </button>
-          <button
-            class="button TimeGraph mt-5 ml-3"
-            onClick={() => setTimeFrame("7d")}
-          >
-            7d
-          </button>
+          <div>
+            <button
+              class="button TimeGraph mt-5"
+              onClick={() => setTimeFrame("24h")}
+            >
+              24h
+            </button>
+            <button
+              class="button TimeGraph mt-5 ml-3"
+              onClick={() => setTimeFrame("7d")}
+            >
+              7d
+            </button>
+            <button
+              class="button TimeGraph mt-5 ml-3"
+              onClick={() => setTimeFrame("1m")}
+            >
+              1m
+            </button>
+            <button
+              class="button TimeGraph mt-5 ml-3"
+              onClick={() => setTimeFrame("3m")}
+            >
+              3m
+            </button>
+            <button
+              class="button TimeGraph mt-5 ml-3"
+              onClick={() => setTimeFrame("1y")}
+            >
+              1y
+            </button>
+          </div>
+          <div>
+            <button
+              class={`button TimeGraph2 mt-5 ${
+                chartType === "price" ? "active" : ""
+              }`}
+              onClick={() => setChartType("price")}
+            >
+              Price
+            </button>
+            <button
+              class={`button TimeGraph2 mt-5 ml-2 ${
+                chartType === "marketCap" ? "active" : ""
+              }`}
+              onClick={() => setChartType("marketCap")}
+            >
+              Market Cap
+            </button>
+          </div>
         </div>
         <div className="relative top-[30vh]">
           <div className="w-full h-96 rounded-lg shadow">
             <ResponsiveContainer width="100%" height="100%">
-              <LineChart data={chartData2}>
-                <XAxis
-                  dataKey="time"
-                  tickFormatter={formatXAxis}
-                  // interval="preserveStartEnd"
-                  minTickGap={30}
-                  tick={{ fontSize: 10 }}
-                  domain={xDomain}
-                />
-                <YAxis
-                  tickCount={9}
-                  tickValues={tickValuesLowPrices}
-                  tickFormatter={(value) => {
-                    if (value < 0.01) {
-                      return `$${value.toFixed(10)}`;
-                    } else if (value < 1) {
-                      return `$${value.toFixed(2)}`;
-                    } else {
-                      return `$${(value / 1000).toFixed(1)}K`;
+              {chartType === "price" ? (
+                <LineChart data={chartData2}>
+                  <XAxis
+                    dataKey="time"
+                    tickFormatter={formatXAxis}
+                    minTickGap={30}
+                    tick={{ fontSize: 10 }}
+                    domain={xDomain}
+                  />
+                  <YAxis
+                    tickCount={9}
+                    tickValues={tickValuesLowPrices}
+                    tickFormatter={(value) => {
+                      if (value < 0.01) {
+                        return `$${value.toFixed(10)}`;
+                      } else if (value < 1) {
+                        return `$${value.toFixed(2)}`;
+                      } else {
+                        return `$${(value / 1000).toFixed(1)}K`;
+                      }
+                    }}
+                    domain={[lowestPrice, highestPrice]}
+                  />
+                  <Tooltip content={<CustomTooltip />} />
+                  <Line
+                    type="monotone"
+                    dataKey="price"
+                    stroke={priceChange24h >= 0 ? "#00FF00" : "#FF0000"}
+                    className={
+                      priceChange24h >= 0
+                        ? "blink-greensingle"
+                        : "blink-redsingle"
                     }
+                    dot={false}
+                    strokeWidth={2}
+                  />
+                </LineChart>
+              ) : (
+                <LineChart
+                  width={500}
+                  height={300}
+                  data={CoinMcapDetails}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
                   }}
-                  domain={[lowestPrice, highestPrice]}
-                />
-                <Tooltip content={<CustomTooltip />} />
-                <Line
-                  type="monotone"
-                  dataKey="price"
-                  stroke={priceChange24h >= 0 ? "#00FF00" : "#FF0000"}
-                  className={
-                    priceChange24h >= 0
-                      ? "blink-greensingle"
-                      : "blink-redsingle"
-                  }
-                  dot={false}
-                  strokeWidth={2}
-                />
-              </LineChart>
+                >
+                  <XAxis
+                    dataKey="0"
+                    tickFormatter={(unixTimestamp) => {
+                      if (timeFrame === "1m") {
+                        const date = new Date(unixTimestamp);
+                        return date.toLocaleDateString("en-US", {
+                          month: "short",
+                          day: "2-digit",
+                        });
+                      } else if (timeFrame === "7d") {
+                        const date = new Date(unixTimestamp);
+                        return date.toLocaleDateString("en-US", {
+                          month: "short",
+                          day: "2-digit",
+                        });
+                      } else if (timeFrame === "3m") {
+                        const date = new Date(unixTimestamp);
+                        return date.toLocaleDateString("en-US", {
+                          month: "short",
+                          year: "numeric",
+                        });
+                      } else if (timeFrame === "1y") {
+                        const date = new Date(unixTimestamp);
+                        return `${date.toLocaleDateString("en-US", {
+                          year: "numeric",
+                          month: "short",
+                          day: "2-digit",
+                        })} ${date.toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}`;
+                      } else {
+                        const date = new Date(unixTimestamp);
+                        return date.toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        });
+                      }
+                    }}
+                    ticks={CoinMcapDetails.map((item, index) => {
+                      if (timeFrame === "1m") {
+                        return index % 6 === 0 ? item[0] : null; // 8 days gap
+                      } else if (timeFrame === "7d") {
+                        return index % 2 === 0 ? item[0] : null; // 2 days gap
+                      } else if (timeFrame === "3m") {
+                        return index % 20 === 0 ? item[0] : null; // 1 month gap
+                      } else if (timeFrame === "1y") {
+                        return index % 50 === 0 ? item[0] : null; // 1 year gap
+                      } else {
+                        return item[0];
+                      }
+                    })}
+                    tick={{ fontSize: 10 }}
+                    domain={xAxisDomain}
+                  />
+                  <YAxis
+                    tickCount={9}
+                    tickFormatter={(value) => {
+                      if (value >= 1e12) {
+                        return `$${(value / 1e12).toFixed(2)}T`;
+                      } else if (value >= 1e9) {
+                        return `$${(value / 1e9).toFixed(2)}B`;
+                      } else if (value >= 1e6) {
+                        return `$${(value / 1e6).toFixed(2)}M`;
+                      } else if (value >= 1e3) {
+                        return `$${(value / 1e3).toFixed(2)}K`;
+                      } else if (value < 1 && value >= 0.001) {
+                        return `$${value.toFixed(6)}`;
+                      } else if (value < 0.001 && value >= 0.000001) {
+                        return `$${value.toFixed(8)}`;
+                      } else if (value < 0.000001 && value >= 0.00000001) {
+                        return `$${value.toFixed(10)}`;
+                      } else {
+                        return `$${value.toFixed(2)}`;
+                      }
+                    }}
+                    domain={[lowestMarketCap, highestMarketCap]}
+                  />
+                  <Tooltip
+                    content={<CustomTooltip />}
+                    label={CoinMcapDetails[0][0]}
+                  />
+                  <Line
+                    type="monotone"
+                    dataKey="1"
+                    stroke="#8884d8"
+                    dot={false}
+                    strokeWidth={2}
+                  />
+                </LineChart>
+              )}
             </ResponsiveContainer>
           </div>
         </div>
